86e4846b7d704bc8741634e382bcde6ca80eb78f
Кибакин Виктор <v.kibakin@yandex.ru>
2025-08-10 12:30:57 +0300

    Changed formating

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,14 @@
 from random import randint
 
 
-correct_number = randint(1,100)
+correct_number = randint(1, 100)
 print('Угадай число от 1 до 100')
 
 while True:
     guess = int(input('Введите число: '))
 
     if guess < correct_number:
-        print(f'Загаданное число больше чем {guess}')    
+        print(f'Загаданное число больше чем {guess}')
     elif guess > correct_number:
         print(f'Загаданное число меньше чем {guess}')
     elif guess == correct_number:
